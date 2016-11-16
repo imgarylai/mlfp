1dfdd07bd12648ed8e232c51e802c6116acba23f
bzuo <bzuo@users.noreply.github.com>
2016-11-16 01:46:39 -0500

    Delete PopFunction.m

--- PopFunction.m
+++
@@ -1,7 +0,0 @@
-function [pop] = PopFunction(GenomeLength,~,options)
-% Generate selected feature index as a logical vector 
-% For exmaple, [0 1 0 0 1 ..... 1 1 0 0]
-rand('seed',1);
-RD = rand;  
-pop = (rand(options.PopulationSize, GenomeLength)> RD); % Initial Population
-end
