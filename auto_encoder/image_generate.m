53730ee9141ef4b6a56639538bd1765a061ecabb
Gary Lai <garylai1990@gmail.com>
2016-11-26 20:06:02 -0500

    clinical-mse
    Former-commit-id: c1854fb959c27b97f7bfb0e75c8ce2db19bbc8cc

--- auto_encoder/image_generate.m
+++ auto_encoder/image_generate.m
@@ -1,19 +1,23 @@
 load 'data/clean/clean.mat';
-T = 10;
+T = 5;
 X = clinical_train.Features;
 [featureSize, sampleSize] = size(X);
+mse = zeros(T, featureSize);
+
 
 for t = 1:T
+    disp('-------------------------------------');
+    fprintf('Interation: %d. \n', t);
+    disp('-------------------------------------');
     for i=1:featureSize
         [newFeatures, autoenc, mseError]= ml_autoencoder(X, i);
-        if(t == 1)
-            [newFeatureSize, newSampleSize] = size(newFeatures);
-            v.features = newFeatures;
-            v.autoencoder = autoenc;
-            fname = sprintf('auto_encoder/generated_data/autoencoder-%s-%d-%d.mat', char(clinical_train.SymbolTypes(1)) ,featureSize, newFeatureSize);
-            save(fname, '-struct', 'v'); 
-        end
-        
+%         if(t == 1)
+%             [newFeatureSize, newSampleSize] = size(newFeatures);
+%             v.features = newFeatures;
+%             v.autoencoder = autoenc;
+%             fname = sprintf('auto_encoder/generated_data/autoencoder-%s-%d-%d.mat', char(clinical_train.SymbolTypes(1)) ,featureSize, newFeatureSize);
+%             save(fname, '-struct', 'v'); 
+%         end 
         mse(t, i) = mseError;
     end
 end
