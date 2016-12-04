324814e41db1c1ed00ab097b8a1c565c58acd247
Gary Lai <garylai1990@gmail.com>
2016-12-04 17:24:32 -0500

    write a main function

---
+++ auto_encoder/image_generator.m
@@ -0,0 +1,27 @@
+function image_generator(type)
+    T = 5;
+    X = type.Features;
+    [featureSize, ~] = size(X);
+    mse = zeros(T, featureSize);
+    save_path = sprintf('auto_encoder/generated_data/%s.mat', char(type.SymbolTypes(1)));
+
+    for t = 1:T
+        disp('-------------------------------------');
+        fprintf('Interation: %d. \n', t);
+        disp('-------------------------------------');
+        for i=1:featureSize
+            [newFeatures, autoenc, mseError]= ml_autoencoder(X, i);
+    %         if(t == 1)
+    %             [newFeatureSize, newSampleSize] = size(newFeatures);
+    %             v.features = newFeatures;
+    %             v.autoencoder = autoenc;
+    %             fname = sprintf('generated_data/autoencoder-%s-%d-%d.mat', char(type.SymbolTypes(1)) ,featureSize, newFeatureSize);
+    %             save(fname, '-struct', 'v'); 
+    %         end 
+            mse(t, i) = mseError;
+            save(save_path , 'mse');
+        end
+    end
+
+end
+
